015d1db091168c80414340f6710649f076b71dce
DheerajMadda47 <dheeraj.madda@tigeranalytics.com>
2022-07-11 21:36:31 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 import redis
 import pandas as pd
 # import asyncio
-
 # import aioredis
 
 app = FastAPI()
@@ -10,16 +9,23 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def startup_event():
-    global r
-    r = redis.Redis(
-        host="demo1-redis.redis.cache.windows.net",
-        port="6379",
-        password="L9c5g7iyu9iLreYHQr2nQGXpi7tVhEixoAzCaAg5qVA=",
-    )
+    try:
+        global r
+        r = redis.Redis(
+            host="demo1redis.redis.cache.windows.net",
+            port="6379",
+            password="uQZ4WmnYA3CjhXXi3DtrnH1m0CHbGgqQDAzCaNPayYM=",
+        )
+    except:
+        pass
 
-@app.post("/get_cache")
+@app.get("/get_cache")
 async def root():
     start_time = pd.Timestamp.now()
-    value_ = r.hget('key1', 'value1')
+    try:
+        value_ = r.hget('key1', 'value1')
+    except:
+        value_ = str(start_time).encode()
+        
     print(f"Time taken for preprocessing: {pd.Timestamp.now() - start_time}")
     return {"message": value_.decode()}
